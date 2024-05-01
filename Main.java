914972f76c3b37fb73341ff246a0e62bf85ff4ab
abulebbeh <abulebbehdoaa@gmail.com>
2024-04-30 18:52:13 -0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,62 +1,11 @@
-import java.util.Scanner;
-
-/*1. Book Class
- * Fields:
- *  -private String title, author, ISBN;
- *  -private boolean isAvailable
- * Constructor:
- *  Book(title, author, ISBN, isAvailable)
- * Methods:   
- *  getTitle()
- *  setTitle(newTitle) 
- *  getAuthor()
- *  setAuthor(newAuthor)
- *  getISBN()
- *  setISBN(newISBN)
- *  isAvailable()
- *  setAvailable(availability)
- * 
- * 2. LibraryItem Interface 
- * Methods:
- *  checkout()
- *  returnItem()
- *  getStatus()
- * 
- * 3. Inheritable Classes
- * FictionBook extends Book
- *  Extra Field: private String genre;
- *  Methods:
- *  displayDetails() //Override to include subject
- * 
- * 4. LibraryCatalog Class
- * Fields:
- *  private ArrayList books;
- * Constructor: LibraryCatalog()
- * Methods:
- *  addBook(Book book)
- *  removeBookByISBN(String ISBN)
- *  searchBookByTitle(String title)
- *  checkout(String ISBN) //Implements LibraryItem
- *  returnItem(String ISBN) //Implements LibraryItem
- *  getStatus(String ISBN) //Implements LibraryItem
- * 
- * ***Enhanced LibraryCatalog Class with User Input Method
- * 1. AddBookViaUserInput() Method:
- *  Prompt the user for book details (title, author, ISBN, availability, and if applicable, genre or subject based on the book type)
- *  Create a Book, FictionBook, or NonFictionBook object based on the input
- *  Add the created book object to the ArrayList<Book>
- * 2. Integration in the Main Application
- *  Provide a menu option for the user to select if they want to add a book
- *  Based on the user's choice, invoke addBookViaUserInput()
- * 
- * 
- * 5. Main Application
- * Flow: 
- *  Create Book, FictionBook, and NonFictionBook objects
- *  Add to LibraryCatalog
- *  Perform a checkout and return operation
- *  Display status after operations
+/*
+ * Doaa Abulebbeh
+ * Professor Allos
+ * CSIS 294 – INTERM JAVA PROG/DATA STRUCT
+ * Lab Exercise: Implementing a Simple Library Management System in Java
  */
+
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
